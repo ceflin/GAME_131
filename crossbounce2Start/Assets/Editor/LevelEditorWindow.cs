23f5c657479a357a0bfd41d1b0eaf9561fedd83f
ceflin <ceflin@stumail.jccc.edu>
2018-04-10 19:23:11 -0500

    Added a demo and new work for next project.

---
+++ crossbounce2Start/Assets/Editor/LevelEditorWindow.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+
+public class LevelEditorWindow : EditorWindow
+{
+    [MenuItem("Tools/Level Creator/Show Level Editor")]
+    private static void ShowLevelEditor()
+    {
+        LevelEditorWindow.ShowWindow();
+    }
+    private static LevelEditorWindow instance;
+
+    public static void ShowWindow()
+    {
+        instance = EditorWindow.GetWindow<LevelEditorWindow>();
+        instance.titleContent = new GUIContent("Level Editor");
+    }
+
+    private float currentX = 0f;
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Create Wall"))
+        {
+            string[] cubeGuids = AssetDatabase.FindAssets("wallBasePrefab");
+
+            StringBuilder guidBuilder = new StringBuilder();
+            foreach (string cubeGuid in cubeGuids)
+            {
+                guidBuilder.AppendLine(cubeGuid);
+            }
+            UnityEngine.MonoBehaviour.print(guidBuilder.ToString());
+
+            if (cubeGuids.Length > 0)
+            {
+                string trueCubeGuid = cubeGuids[0];
+
+                string assetPath = AssetDatabase.GUIDToAssetPath(trueCubeGuid);
+                UnityEngine.MonoBehaviour.print(assetPath);
+
+
+                GameObject cubeTemplate = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
+
+                GameObject newCube = GameObject.Instantiate(cubeTemplate);
+                newCube.name = cubeTemplate.name;
+
+                Vector3 newCubePos = newCube.transform.position;
+                newCubePos.x = currentX;
+                newCube.transform.position = newCubePos;
+
+                currentX += 1f;
+            }
+        }
+        if (GUILayout.Button("Create Target"))
+        {
+            string[] cubeGuids = AssetDatabase.FindAssets("Target");
+
+            StringBuilder guidBuilder = new StringBuilder();
+            foreach (string cubeGuid in cubeGuids)
+            {
+                guidBuilder.AppendLine(cubeGuid);
+            }
+            UnityEngine.MonoBehaviour.print(guidBuilder.ToString());
+
+            if (cubeGuids.Length > 0)
+            {
+                string trueCubeGuid = cubeGuids[0];
+
+                string assetPath = AssetDatabase.GUIDToAssetPath(trueCubeGuid);
+                UnityEngine.MonoBehaviour.print(assetPath);
+
+
+                GameObject targetTemplate = AssetDatabase.LoadAssetAtPath(assetPath, typeof(GameObject)) as GameObject;
+
+                GameObject newTarget = GameObject.Instantiate(targetTemplate);
+                newTarget.name = targetTemplate.name;
+
+                Vector3 newTargetPos = newTarget.transform.position;
+                newTargetPos.x = currentX;
+                newTarget.transform.position = newTargetPos;
+
+                currentX += 1f;
+            }
+        }
+    }
+}
