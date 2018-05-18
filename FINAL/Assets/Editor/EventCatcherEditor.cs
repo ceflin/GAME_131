775fd3e5708a8d90e4bc27c5d1f23aee1bb6fdd7
ceflin <ceflin@stumail.jccc.edu>
2018-05-17 20:44:18 -0500

    work for FINAL

---
+++ FINAL/Assets/Editor/EventCatcherEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EventCatcherEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+
+    private void print(object message)
+    {
+        UnityEngine.MonoBehaviour.print(message);
+    }
+
+    private void OnSceneGUI()
+    {
+        Event currentEvent = Event.current;
+
+        //switch (currentEvent.type)
+        //{
+        //    case EventType.KeyDown:
+        //        if (currentEvent.keyCode == KeyCode.E)
+        //        {
+        //            UnityEngine.MonoBehaviour.print("Mouse Position: " + Event.current.mousePosition);
+        //        }
+        //        break;
+        //}
+
+        if (currentEvent.type == EventType.KeyDown)
+        {
+            if (currentEvent.keyCode == KeyCode.E)
+            {
+                //Ray worldRay = HandleUtility.GUIPointToWorldRay(currentEvent.mousePosition);
+                //RaycastHit hitInfo;
+
+                //if (Physics.Raycast(worldRay,out hitInfo))
+                //{
+                //    currentEvent.Use();
+                //    print("mouse pos: " + hitInfo);
+                //}
+                //currentEvent.Use();
+                Vector2 mousePos = Event.current.mousePosition;
+                mousePos.y = Camera.current.pixelHeight - mousePos.y;
+                Vector3 pos = Camera.current.ScreenToWorldPoint(currentEvent.mousePosition);
+                UnityEngine.MonoBehaviour.print(pos);
+            }
+        }
+    }
+}
---
+++ FINAL/Assets/Editor/MenuItems.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuItems : Editor
+{
+    //[MenuItem("Tools/Positions/Show Mouse Position")]
+    //private static void ShowMousePosition()
+    //{
+    //    MousePositionWindow.ShowWindow();
+    //}
+}
